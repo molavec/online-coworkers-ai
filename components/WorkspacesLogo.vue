4eb01a91c8301b95c7d2d75f68be3b488c35ce52
Miguel Olave <molavec@gmail.com>
2025-05-19 00:44:29 -0400

    ajusta el logo

--- components/WorkspacesLogo.vue
+++ components/WorkspacesLogo.vue
@@ -2,8 +2,12 @@
 
 <template>
   <div class="logo">
-    <h1 class="text-2xl text-white">Workspaces</h1>
-    <p>by Queonda.<span class="text-queonda-500">ART</span></p>
+    <h1 class="text-2xl">Workspaces</h1>
+    <p class="ml-3 font-light">
+      by 
+      <span class="font-semibold">
+        Queonda.<span class="text-queonda-red-500">ART</span>
+      </span></p>
   </div>
 </template>
 
